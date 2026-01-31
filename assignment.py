f2a43f2d2116ccfb664722ab5f957b7004f8bb8e
Georgia Nerantzaki <geoneran2000@gmail.com>
2026-01-31 16:59:03 +0200

    improved visualizations

--- assignment.py
+++ assignment.py
@@ -13,29 +13,71 @@ df = pd.read_excel(excel, sheet_name=0, header=0)
 df.to_csv("online_retail.csv", index=False)
 #convert it to a dataframe by reading the csv
 dt = pd.read_csv("./online_retail.csv", index_col=False)
+
+#PHASE 2
 #find number of rows and columns, missing values, data types
 print("Rows and columns: \n", dt.shape)
 print("Missing values: \n", dt.isnull().sum())
 print("Data types in the dataset: \n", dt.dtypes)
 #summary statistics to show any any anomalies like negative values or extremely high ones
 print("Columns: \n",dt.columns)
-print("Summary statistics: \n", dt.describe())
+print("Summary statistics: \n", dt.describe().round(2))
 
 
-#finding outliers 
-plt.figure(figsize=(30,20))
-quantity_boxplot = plt.boxplot(dt['Quantity'])
-plt.title('Boxplot of quantity ')
-plt.show()
-plt.figure(figsize=(30,20))
-unit_price_boxplot = plt.boxplot(dt['UnitPrice'])
-plt.title('Boxplot of quantity ')
-plt.show()
-#barplot with country
-plt.figure(figsize=(30,20))
+#visualizations
+#barplot showing trnascactions per country (all countries)
 number_of_transcactions = dt['Country'].value_counts()
-print("number of transcactions: ", number_of_transcactions)
 country_barplot = plt.bar(number_of_transcactions.index, number_of_transcactions.values)
-plt.title('Barplot of country ')
-country_barplot.xticks(rotate = 90)
+plt.title('Barplot of transcactions per country (all countries)')
+plt.xticks(rotation = 90)
 plt.show()
+
+#barplot showing trnascactions per country (for countries: Germany, France, Norway, Netherlands, Ireland)
+countries  = ['Germany', 'France', 'Norway', 'Netherlands', 'Ireland']
+filtered_dt = dt[dt['Country'].isin(countries)]
+number_of_transcactions = filtered_dt['Country'].value_counts()
+country_barplot = plt.bar(number_of_transcactions.index, number_of_transcactions.values)
+plt.title('Barplot of transcactions per country (Germany, France, Norway, Netherlands, Ireland) ')
+plt.xticks(rotation = 90)
+plt.show()
+
+
+#barplot showing number of unique customers per country (all countries)
+number_of_unique_customers_per_country = dt.groupby('Country')['CustomerID'].nunique()
+country_barplot = plt.bar(number_of_unique_customers_per_country.index, number_of_unique_customers_per_country.values)
+plt.title('Barplot of number of unique customers per country (all countries)')
+plt.xticks(rotation = 90)
+plt.show()
+
+#barplot showing number of unique customers per country (Germany, France, Norway, Netherlands, Ireland)
+countries  = ['Germany', 'France', 'Norway', 'Netherlands', 'Ireland']
+filtered_dt = dt[dt['Country'].isin(countries)]
+number_of_unique_customers = filtered_dt.groupby('Country')['CustomerID'].nunique()
+country_barplot = plt.bar(number_of_unique_customers.index, number_of_unique_customers.values)
+plt.title('Barplot of number of unique customers per country (Germany, France, Norway, Netherlands, Ireland)')
+plt.xticks(rotation = 90)
+plt.show()
+
+
+#barplot showing average quantity per country (Germany, France, Norway, Netherlands, Ireland)
+countries  = ['Germany', 'France', 'Norway', 'Netherlands', 'Ireland']
+filtered_dt = dt[dt['Country'].isin(countries)]
+average_quantity_per_country = filtered_dt.groupby('Country')['Quantity'].mean()
+country_barplot = plt.bar(average_quantity_per_country.index, average_quantity_per_country.values)
+plt.title('Barplot of average quantity per country (Germany, France, Norway, Netherlands, Ireland)')
+plt.xticks(rotation = 90)
+plt.show()
+
+
+
+#barplot showing average unit price per country (Germany, France, Norway, Netherlands, Ireland)
+countries  = ['Germany', 'France', 'Norway', 'Netherlands', 'Ireland']
+filtered_dt = dt[dt['Country'].isin(countries)]
+average_unit_price_per_country = filtered_dt.groupby('Country')['UnitPrice'].mean()
+country_barplot = plt.bar(average_unit_price_per_country.index, average_unit_price_per_country.values)
+plt.title('Barplot of average unit price per country (Germany, France, Norway, Netherlands, Ireland)')
+plt.xticks(rotation = 90)
+plt.show()
+
+
+
